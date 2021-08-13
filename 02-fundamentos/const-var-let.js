f3f6acc9e354065f27857c761ee7c9526aac7923
Federico Arzuaga <fede.arzuaga.perdomo@gmail.com>
2021-08-13 16:56:06 -0300

    Adding the first files on the course folder. I love you node <3

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,15 @@
+let nombre = "Federico";
+
+if (true) {
+    let nombre = "Pedrito";
+    if (true) {
+        let nombre = "Elpepe";
+    }
+    console.log(nombre);
+}
+
+console.log(nombre);
+
+/* ==========================
+      NUNCA USAR VAR!!
+========================== */
