c574ac72775802f4a1893d7c250eab1e23d78e35
Federico Arzuaga <federico.arzuaga@wundermanthomspon.com>
2021-08-17 11:44:00 -0300

    Adding new files into 03-basics folder, as well as a gitignore file

--- 03-basics/app.js
+++ 03-basics/app.js
@@ -1,18 +1,19 @@
 // Requerir paquetes
 const { createFile } = require('./helpers/multiply.js');
+const yargs = require('yargs').argv;
 
 console.clear();
 
-const [ , , totalString = 'base=5'] = process.argv;
-const [ , baseString = 5] = totalString.split('=');
-const base = parseInt(baseString);
+console.log( process.argv );
+console.log( yargs );
 
-createFile( base )
-    .then( output => console.log(output) )
-    .catch( err => console.log(err) );
+console.log( yargs.base );
 
+// NO HACER
+// const [ , , totalString = 'base=5'] = process.argv;
+// const [ , baseString = 5] = totalString.split('=');
+// const base = parseInt(baseString);
 
-
-
-
-
+// createFile( base )
+//     .then( output => console.log(output) )
+//     .catch( err => console.log(err) )
