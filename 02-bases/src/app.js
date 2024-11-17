7c7603a4b01ce15af1cefd802b707d3cbbe65c08
Federico Arzuaga <fede.arzuaga.perdomo@gmail.com>
2024-11-17 09:47:05 -0300

    Nodemon and how to install npm packages learned. Added some more files and folders to the .gitignore file

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,3 +1,3 @@
 const { emailTemplate } = require("./js-foundation/01-template");
 
-console.log( emailTemplate );
+// console.log( emailTemplate );
--- 02-bases/src/js-foundation/01-template.js
+++ 02-bases/src/js-foundation/01-template.js
@@ -2,6 +2,7 @@ const emailTemplate = `
 <div>
     <h1>Hi, {{name}}</h1>
     <p>Thank you for your order.</p>
+    <p>A new paragraph</p>
 </div>`;
 
 module.exports = {
